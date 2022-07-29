dd8f4f48120bda1bd3069664ee6c4376898ec401
Julián Campos <m02capej@uco.es>
2022-07-29 16:07:53 +0200

    exercise 7

---
+++ Exercise 7/exercise-7-1.js
@@ -0,0 +1,13 @@
+/*
+Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
+
+- Un nuevo Set con los nombres de tu familia
+
+- Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+
+- Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+*/
+
+let familia = new Set(['Julian padre', 'Carmen', 'Juan Antonio', 'Julián hijo']);
+familia.add('Carmen');
+familia.add('JavaScript');
