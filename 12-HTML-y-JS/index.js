f0e2ad857d9ad6326666037c4e670aaec5fb3a77
Julián Campos <m02capej@uco.es>
2022-08-16 21:38:37 +0200

    update html y js

---
+++ 12-HTML-y-JS/index.js
@@ -0,0 +1,7 @@
+const a = 4
+const b = 8
+
+console.log(a*b)
+
+const h = document.getElementById("titulo") //dará null porque el elemento cion id titulo se declara después de incorporar el archivo js
+console.log(h)
