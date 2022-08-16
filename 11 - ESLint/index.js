c9ad71e5a8b0921a6bead1cb0f4f213132a1a7a8
Julián Campos <m02capej@uco.es>
2022-08-16 21:19:53 +0200

    estlint

---
+++ 11 - ESLint/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+const nombre = "Julian"
+
+  var nombre2 = "Manolo"
+
+  /* eslint-enable */
+
+  const nuevoString = 'Esto es un nuevo string' // eslint-disable-line
+
+
+  // Hacer que todos los archivos del proyecto sean afectados por las reglas de eslint
+  // e incluso arreglar de forma auto
+
+  //nos vamos al package.json a la parte de scripts y luego lo mostrará por terminal los errores
+  //    "lint": "eslint .", para buscar errores
+  //"lint-fix": "eslint --fix"  para arreglar
